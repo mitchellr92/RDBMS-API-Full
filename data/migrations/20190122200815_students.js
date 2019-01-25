c6df8fbac5099ec83b1290998fe8a2c051b548cd
Mitchell Robles <mitchellr92@live.com>
2019-01-24 18:25:25 -0800

    Created tables for cohorts and students

--- data/migrations/20190122200815_students.js
+++ data/migrations/20190122200815_students.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('students', table => {
+        table.increments();
+        table.string('name');
+        table.integer('cohort_id');
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('students');
 };
